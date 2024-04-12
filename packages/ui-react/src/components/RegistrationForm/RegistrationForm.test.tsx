85d4002804f7dcb60a76c9be093296b0a5d7db4f
mirovladimitrovski <vladimir.dimitrovski@jwplayer.com>
2024-04-12 12:56:27 +0200

    fix: mock social button

--- packages/ui-react/src/components/RegistrationForm/RegistrationForm.test.tsx
+++ packages/ui-react/src/components/RegistrationForm/RegistrationForm.test.tsx
@@ -4,6 +4,14 @@ import { renderWithRouter, waitForWithFakeTimers } from '../../../test/utils';
 
 import RegistrationForm from './RegistrationForm';
 
+// The SocialButton component contains an SVG import that results in an absolute path on the current machine
+// This results in snapshot inconsistencies per machine
+vi.mock('../SocialButton/SocialButton.tsx', () => ({
+  default: (props: { href: string }) => {
+    return <a href={props.href}>Social Button</a>;
+  },
+}));
+
 const socialLoginURLs = {
   twitter: 'https://staging-v2.inplayer.com/',
   facebook: 'https://www.facebook.com/',
