b133fb7b22438ec70d130858699807006c61e49c
Melissa 't Hart <melissa@videodock.com>
2024-05-29 15:22:33 +0200

    chore(project): remove redundant wcag unit test

--- packages/ui-react/src/components/TruncatedText/TruncatedText.test.tsx
+++ packages/ui-react/src/components/TruncatedText/TruncatedText.test.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { axe } from 'vitest-axe';
 import { render } from '@testing-library/react';
 
 import TruncatedText from './TruncatedText';
@@ -9,11 +8,5 @@ describe('<TruncatedText>', () => {
     const { container } = render(<TruncatedText text="Test..." maximumLines={8} />);
 
     expect(container).toMatchSnapshot();
-  });
-
-  test('WCAG 2.2 (AA) compliant', async () => {
-    const { container } = render(<TruncatedText text="Test..." maximumLines={8} />);
-
-    expect(await axe(container, { runOnly: ['wcag21a', 'wcag21aa', 'wcag22aa'] })).toHaveNoViolations();
   });
 });
