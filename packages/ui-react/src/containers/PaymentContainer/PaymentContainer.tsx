d9ea8e8226383777305db5d83780e389e5016b7b
Christiaan Scheermeijer <christiaan@videodock.com>
2024-06-12 10:50:10 +0200

    fix: use correct payment method for google iap

--- packages/ui-react/src/containers/PaymentContainer/PaymentContainer.tsx
+++ packages/ui-react/src/containers/PaymentContainer/PaymentContainer.tsx
@@ -44,10 +44,10 @@ const processBillingReceipt = (receipt: Blob | string, transactionId: string) =>
   }
 };
 
-const EXTERNAL_PAYMENT_METHODS = ['Apple In-App', 'Android In-App', 'Roku In-App'];
+const EXTERNAL_PAYMENT_METHODS = ['Apple In-App', 'Google In-App', 'Roku In-App'];
 const STORE_LINKS: Record<string, string> = {
   apple: 'https://support.apple.com/118428',
-  android: 'https://support.google.com/googleplay/answer/7018481',
+  google: 'https://support.google.com/googleplay/answer/7018481',
   roku: 'https://support.roku.com/article/208756478',
 };
 
