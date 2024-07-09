bbb09bdefc0adb56925ef975eb0d9506f9338f2c
mirovladimitrovski <vladimir.dimitrovski@jwplayer.com>
2024-07-09 13:02:13 +0200

    chore: reorder members of jwp api service class

--- packages/common/src/services/integrations/jwp/JWPAPIService.ts
+++ packages/common/src/services/integrations/jwp/JWPAPIService.ts
@@ -24,21 +24,17 @@ type RequestOptions = {
 export default class JWPAPIService {
   private readonly storageService: StorageService;
 
-  private sandbox = true;
-
-  static isCommonError = (error: unknown): error is JWPError => {
-    return (
-      typeof error === 'object' &&
-      error !== null &&
-      'response' in error &&
-      typeof (error as JWPError).response?.data?.code === 'number' &&
-      typeof (error as JWPError).response?.data?.message === 'string'
-    );
-  };
+  private useSandboxEnv = true;
 
   constructor(@inject(StorageService) storageService: StorageService) {
     this.storageService = storageService;
   }
+
+  setup = (useSandboxEnv: boolean) => {
+    this.useSandboxEnv = useSandboxEnv;
+  };
+
+  private getBaseUrl = () => (this.useSandboxEnv ? 'https://staging-v2.inplayer.com' : 'http://sims.jwplayer.com');
 
   setToken = (token: string, refreshToken = '', expires: number) => {
     return this.storageService.setItem(INPLAYER_TOKEN_KEY, JSON.stringify({ token, refreshToken, expires }), false);
@@ -63,8 +59,6 @@ export default class JWPAPIService {
 
     return !!tokenObject.token && tokenObject.expires > Date.now() / 1000;
   };
-
-  private getBaseUrl = () => (this.sandbox ? 'https://staging-v2.inplayer.com' : 'http://sims.jwplayer.com');
 
   private performRequest = async (
     path: string = '/',
@@ -125,10 +119,6 @@ export default class JWPAPIService {
     return resParsed;
   };
 
-  setup = (sandbox: boolean) => {
-    this.sandbox = sandbox;
-  };
-
   get = <T>(path: string, options?: RequestOptions, searchParams?: Record<string, string | number>) =>
     this.performRequest(path, 'GET', undefined, options, searchParams) as Promise<T>;
 
@@ -139,4 +129,14 @@ export default class JWPAPIService {
   post = <T>(path: string, body?: Record<string, unknown>, options?: RequestOptions) => this.performRequest(path, 'POST', body, options) as Promise<T>;
 
   remove = <T>(path: string, options?: RequestOptions, body?: Record<string, unknown>) => this.performRequest(path, 'DELETE', body, options) as Promise<T>;
+
+  static isCommonError = (error: unknown): error is JWPError => {
+    return (
+      typeof error === 'object' &&
+      error !== null &&
+      'response' in error &&
+      typeof (error as JWPError).response?.data?.code === 'number' &&
+      typeof (error as JWPError).response?.data?.message === 'string'
+    );
+  };
 }
