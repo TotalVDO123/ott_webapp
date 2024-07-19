3fc50a48099eb96678c12a5906c920433809869c
“Kire <k_mitrov@yahoo.com>
2024-07-19 14:25:19 +0200

    feat(project): remove not needed ls-lint

--- platforms/access-bridge/.eslintrc.cjs
+++ platforms/access-bridge/.eslintrc.cjs
@@ -2,13 +2,11 @@ module.exports = {
   extends: ['jwp/typescript'],
   rules: {
     "max-len": ["error", { "code": 120 }],
-    "import/no-unused-modules": [
-      "error"
-    ]
+    "import/no-unused-modules": ["error"]
   },
-  ignorePatterns: ['out'],
   env: {
     node: true, // Enables recognition of Node.js global variables and scoping rules
   },
+  ignorePatterns: ['out'],
 };
 
