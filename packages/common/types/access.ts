ed29191159325abdf4cede743ce52dd889ff7f43
“Kire <k_mitrov@yahoo.com>
2024-07-25 14:00:03 +0200

    chore: move types in common

--- packages/common/types/access.ts
+++ packages/common/types/access.ts
@@ -1,3 +1,16 @@
+import type { AccessControlPlan } from './plans';
+
+export type GenerateAccessTokensParams = {
+  siteId: string;
+  viewerId: string;
+  plans: AccessControlPlan[];
+};
+
+export type RefreshAccessTokensParams = {
+  siteId: string;
+  refreshToken: string;
+};
+
 export type AccessTokensResponse = {
   passport: string;
   refresh_token: string;
