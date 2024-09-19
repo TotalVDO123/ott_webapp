e9cde38d58c1cf8a157be7fcf7e3dd273ffbd175
“Kire <k_mitrov@yahoo.com>
2024-09-19 13:56:16 +0200

    feat(project): remove stripe type

--- packages/common/types/stripe.ts
+++
@@ -1,6 +0,0 @@
-import type { CheckoutParams } from './payment';
-
-// Extend the CheckoutParams type with Stripe-specific fields
-export type StripeCheckoutParams = CheckoutParams & {
-  mode: 'payment' | 'subscription';
-};
